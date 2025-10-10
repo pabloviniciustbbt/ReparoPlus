e02b27e822f3b26aa314cae753fcd7a36bc18a7e
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-10-10 14:51:40 -0300

    feat: adiciona construtor em Tecnico com PessoaCreateRequestDTO

--- src/main/java/com/pabloleal/ReparoPlus/models/Tecnico.java
+++ src/main/java/com/pabloleal/ReparoPlus/models/Tecnico.java
@@ -1,5 +1,6 @@
 package com.pabloleal.ReparoPlus.models;
 
+import com.pabloleal.ReparoPlus.dto.PessoaCreateRequestDTO;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -14,14 +15,20 @@ import java.util.Set;
 
 @Entity
 @Table(name = "tecnicos")
-@NoArgsConstructor
 public class Tecnico extends Pessoa {
 
     @OneToMany(mappedBy = "tecnico", fetch = FetchType.LAZY)
     private Set<OrdemServico> ordensServico = new HashSet<>();
 
+    public Tecnico() {
+    }
+
     public Tecnico(String cpf, String nome, String email, String telefone) {
         super(cpf, nome, email, telefone);
+    }
+
+    public Tecnico(PessoaCreateRequestDTO dados) {
+        super(dados);
     }
 
     public Set<OrdemServico> getOrdensServico() {

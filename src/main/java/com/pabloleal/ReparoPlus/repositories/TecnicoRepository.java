9498844bd5c0730105292a4dc292c74a3e0713c4
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-11-05 07:34:36 -0300

    feat: adicionar método findByCpf no repositório de tecnicos

--- src/main/java/com/pabloleal/ReparoPlus/repositories/TecnicoRepository.java
+++ src/main/java/com/pabloleal/ReparoPlus/repositories/TecnicoRepository.java
@@ -3,5 +3,8 @@ package com.pabloleal.ReparoPlus.repositories;
 import com.pabloleal.ReparoPlus.models.Tecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TecnicoRepository extends JpaRepository<Tecnico, Long> {
+    Optional<Tecnico> findByCpf(String cpf);
 }

6febdcc9f53edeb445fda4cb09cc791b36563f6b
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-11-05 07:16:29 -0300

    feat: adicionar método findByCpf no repositório de atendentes

--- src/main/java/com/pabloleal/ReparoPlus/repositories/AtendenteRepository.java
+++ src/main/java/com/pabloleal/ReparoPlus/repositories/AtendenteRepository.java
@@ -3,5 +3,8 @@ package com.pabloleal.ReparoPlus.repositories;
 import com.pabloleal.ReparoPlus.models.Atendente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AtendenteRepository extends JpaRepository<Atendente, Long> {
+    Optional<Atendente> findByCpf(String cpf);
 }

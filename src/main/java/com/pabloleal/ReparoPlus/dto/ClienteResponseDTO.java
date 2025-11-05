f194f500353e5ef0aa73699b43b3193a9ea25062
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-11-05 06:50:04 -0300

    feat: criar DTO de resposta para informações do cliente

--- src/main/java/com/pabloleal/ReparoPlus/dto/ClienteResponseDTO.java
+++ src/main/java/com/pabloleal/ReparoPlus/dto/ClienteResponseDTO.java
@@ -12,4 +12,12 @@ public record ClienteResponseDTO(
         Endereco endereco
 
 ){
+    public ClienteResponseDTO(Long id, String cpf, String nome, String email, String telefone, Endereco endereco) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
 }

7dcb5da19e9515f2c9a0e098bca7da06d92a3c4f
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-11-05 06:50:49 -0300

    feat: adicionar método findByCpf no repositório de clientes

--- src/main/java/com/pabloleal/ReparoPlus/repositories/ClienteRepository.java
+++ src/main/java/com/pabloleal/ReparoPlus/repositories/ClienteRepository.java
@@ -3,7 +3,11 @@ package com.pabloleal.ReparoPlus.repositories;
 import com.pabloleal.ReparoPlus.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
 
     Cliente getReferenceByCpf(String cpf);
+
+    Optional<Cliente> findByCpf(String cpf);
 }

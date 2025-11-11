b13f4ec6a85800d22005193589978e1e6a39a88c
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-11-11 07:22:50 -0300

    feat: implementa lógica de cadastro e validações no serviço

--- src/main/java/com/pabloleal/ReparoPlus/services/OrdemServicoServices.java
+++ src/main/java/com/pabloleal/ReparoPlus/services/OrdemServicoServices.java
@@ -14,8 +14,6 @@ import com.pabloleal.ReparoPlus.repositories.TecnicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class OrdemServicoServices {
 

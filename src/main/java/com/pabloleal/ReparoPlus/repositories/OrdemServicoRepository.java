533db189a90348c5fe71321458459ede423da991
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-12-05 08:01:08 -0300

    feat: adiciona métodos para busca de ordens de serviço por técnico e cliente no repositóriy

--- src/main/java/com/pabloleal/ReparoPlus/repositories/OrdemServicoRepository.java
+++ src/main/java/com/pabloleal/ReparoPlus/repositories/OrdemServicoRepository.java
@@ -1,7 +1,12 @@
 package com.pabloleal.ReparoPlus.repositories;
 
 import com.pabloleal.ReparoPlus.models.OrdemServico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrdemServicoRepository extends JpaRepository<OrdemServico,Long> {
+    Page<OrdemServico> findAllByTecnicoId(Long id, Pageable pageable);
+
+    Page<OrdemServico> findAllByClienteId(Long id, Pageable pageable);
 }

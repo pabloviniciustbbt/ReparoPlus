6f55a07bb1fbb20c2a7cfcc240ef9641c372194b
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-12-08 07:22:35 -0300

    feat: adiciona consultas por status no repositório de ordens de serviço

--- src/main/java/com/pabloleal/ReparoPlus/repositories/OrdemServicoRepository.java
+++ src/main/java/com/pabloleal/ReparoPlus/repositories/OrdemServicoRepository.java
@@ -1,6 +1,7 @@
 package com.pabloleal.ReparoPlus.repositories;
 
 import com.pabloleal.ReparoPlus.models.OrdemServico;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,8 @@ public interface OrdemServicoRepository extends JpaRepository<OrdemServico,Long>
     Page<OrdemServico> findAllByTecnicoId(Long id, Pageable pageable);
 
     Page<OrdemServico> findAllByClienteId(Long id, Pageable pageable);
+
+    Page<OrdemServico> findAllByAtivoFalse(Pageable pageable);
+
+    Page<OrdemServico> findAllByAtivoTrue(Pageable pageable);
 }

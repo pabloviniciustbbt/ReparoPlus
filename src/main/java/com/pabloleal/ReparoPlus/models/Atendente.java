b0c35980c88efcf37a340d929d979eb37a7abba7
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-12-08 08:04:34 -0300

    style: remove import não utilizado na classe Atendente

--- src/main/java/com/pabloleal/ReparoPlus/models/Atendente.java
+++ src/main/java/com/pabloleal/ReparoPlus/models/Atendente.java
@@ -1,31 +1,29 @@
-package com.pabloleal.ReparoPlus.models;
+    package com.pabloleal.ReparoPlus.models;
 
-import com.pabloleal.ReparoPlus.dto.PessoaCreateRequestDTO;
-import jakarta.persistence.*;
-import lombok.NoArgsConstructor;
+    import com.pabloleal.ReparoPlus.dto.PessoaCreateRequestDTO;
+    import jakarta.persistence.*;
+    import java.util.HashSet;
+    import java.util.Set;
 
-import java.util.HashSet;
-import java.util.Set;
+    @Entity
+    @Table(name = "atendentes")
+    public class Atendente extends Pessoa {
 
-@Entity
-@Table(name = "atendentes")
-public class Atendente extends Pessoa {
+        @OneToMany(mappedBy = "atendente", fetch = FetchType.LAZY)
+        private Set<OrdemServico> ordensServico = new HashSet<>();
 
-    @OneToMany(mappedBy = "atendente", fetch = FetchType.LAZY)
-    private Set<OrdemServico> ordensServico = new HashSet<>();
+        public Atendente() {
+        }
 
-    public Atendente() {
+        public Atendente(PessoaCreateRequestDTO pessoaDTO){
+            super(pessoaDTO);
+        }
+
+        public Set<OrdemServico> getOrdensServico() {
+            return ordensServico;
+        }
+
+        public void setOrdensServico(Set<OrdemServico> ordensServico) {
+            this.ordensServico = ordensServico;
+        }
     }
-
-    public Atendente(PessoaCreateRequestDTO pessoaDTO){
-        super(pessoaDTO);
-    }
-
-    public Set<OrdemServico> getOrdensServico() {
-        return ordensServico;
-    }
-
-    public void setOrdensServico(Set<OrdemServico> ordensServico) {
-        this.ordensServico = ordensServico;
-    }
-}

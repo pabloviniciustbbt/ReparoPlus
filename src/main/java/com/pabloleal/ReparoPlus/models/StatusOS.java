60d1b49b028ad09c56b6a885c673749a919acd7a
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2025-12-10 07:41:02 -0300

    feat: adicionar identificador numérico e método de mapeamento ao enum StatusOS

--- src/main/java/com/pabloleal/ReparoPlus/models/StatusOS.java
+++ src/main/java/com/pabloleal/ReparoPlus/models/StatusOS.java
@@ -1,30 +1,46 @@
 package com.pabloleal.ReparoPlus.models;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum StatusOS {
 
-    EM_ORCAMENTO("Em Orçamento"),
-    NAO_APRESENTOU_DEFEITO("Não Apresentou Defeito"),
-    AGUARDANDO_APROVACAO("Aguardando Aprovação"),
-    ORCAMENTO_NAO_APROVADO("Orçamento não Aprovado"),
-    ORCAMENTO_APROVADO("Orçamento Aprovado"),
-    AGUARDANDO_PECA("Aguardando Peça"),
-    EM_EXECUCAO("Em Execução"),
-    DEVOLUCAO_SEM_CONSERTO("Devolução sem Conserto"),
-    CLIENTE_AVISADO_DE_PRONTO("Cliente Avisado de Pronto"),
-    EQUIPAMENTO_RETIRADO("Equipamento Retirado");
+    EM_ORCAMENTO(1, "Em Orçamento"),
+    NAO_APRESENTOU_DEFEITO(2, "Não Apresentou Defeito"),
+    AGUARDANDO_APROVACAO(3, "Aguardando Aprovação"),
+    ORCAMENTO_NAO_APROVADO(4, "Orçamento não Aprovado"),
+    ORCAMENTO_APROVADO(5, "Orçamento Aprovado"),
+    AGUARDANDO_PECA(6, "Aguardando Peça"),
+    EM_EXECUCAO(7, "Em Execução"),
+    DEVOLUCAO_SEM_CONSERTO(8, "Devolução sem Conserto"),
+    CLIENTE_AVISADO_DE_PRONTO(9, "Cliente Avisado de Pronto"),
+    EQUIPAMENTO_RETIRADO(10, "Equipamento Retirado");
 
+    private final int id;
     private final String descricao;
 
-    StatusOS(String descricao) {
+    StatusOS(int id, String descricao) {
+        this.id = id;
         this.descricao = descricao;
     }
 
     public String getDescricao() {
         return descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @JsonCreator
+    public static StatusOS fromId(int id){
+        StatusOS[] list = StatusOS.values();
+
+        for (int i = 0; i < list.length; i++) {
+            StatusOS statusOS = list[i];
+            if (statusOS.id == id){
+                return statusOS;
+            }
+        }
+        throw new IllegalArgumentException("Status OS Inválido: " + id);
     }
 }

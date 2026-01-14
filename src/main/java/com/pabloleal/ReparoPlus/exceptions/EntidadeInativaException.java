10453cb25903ba47c396a56548dfe983fd8ceef6
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2026-01-14 11:26:07 -0300

    feat: adiciona exceção para entidades inativas

---
+++ src/main/java/com/pabloleal/ReparoPlus/exceptions/EntidadeInativaException.java
@@ -0,0 +1,7 @@
+package com.pabloleal.ReparoPlus.exceptions;
+
+public class EntidadeInativaException extends RuntimeException{
+    public EntidadeInativaException(String message) {
+        super(message);
+    }
+}

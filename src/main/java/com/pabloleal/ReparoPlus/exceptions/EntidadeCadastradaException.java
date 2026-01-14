96e2a59104fbe6fb0f4a6872d7ff88efa66cac36
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2026-01-14 11:25:43 -0300

    feat: adiciona exceção para entidades duplicadas

---
+++ src/main/java/com/pabloleal/ReparoPlus/exceptions/EntidadeCadastradaException.java
@@ -0,0 +1,7 @@
+package com.pabloleal.ReparoPlus.exceptions;
+
+public class EntidadeCadastradaException extends RuntimeException{
+    public EntidadeCadastradaException(String message) {
+        super(message);
+    }
+}

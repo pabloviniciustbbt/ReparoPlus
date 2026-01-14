f6e82a2d13d82d5a51872b6eac0fd766ff32d24e
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2026-01-14 11:26:52 -0300

    feat: centraliza tratamento de exceções com GlobalExceptionHandler
    Adiciona @RestControllerAdvice para tratamento global de exceções
    do domínio, padronizando respostas de erro da API

---
+++ src/main/java/com/pabloleal/ReparoPlus/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.pabloleal.ReparoPlus.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(EntityNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(EntidadeInativaException.class)
+    public ResponseEntity tratarEntidadeInativa(EntidadeInativaException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(EntidadeCadastradaException.class)
+    public ResponseEntity tratarEntidadeCadastrada(EntidadeCadastradaException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+}

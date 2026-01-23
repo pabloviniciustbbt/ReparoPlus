88a00827622b00366bb6cb636de04cd35d5e051e
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2026-01-23 08:02:59 -0300

    refactor: filtrar ordens de serviço ativas ao buscar por cliente

--- src/main/java/com/pabloleal/ReparoPlus/repositories/OrdemServicoRepository.java
+++ src/main/java/com/pabloleal/ReparoPlus/repositories/OrdemServicoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface OrdemServicoRepository extends JpaRepository<OrdemServico,Long> {
+public interface OrdemServicoRepository extends JpaRepository<OrdemServico, Long> {
     Page<OrdemServico> findAllByTecnicoId(Long id, Pageable pageable);
 
     Page<OrdemServico> findAllByClienteId(Long id, Pageable pageable);
@@ -15,5 +15,5 @@ public interface OrdemServicoRepository extends JpaRepository<OrdemServico,Long>
 
     Page<OrdemServico> findAllByAtivoTrue(Pageable pageable);
 
-    List<OrdemServico> findAllByClienteId(Long id);
+    List<OrdemServico> findAllByClienteIdAndAtivoTrue(Long id);
 }

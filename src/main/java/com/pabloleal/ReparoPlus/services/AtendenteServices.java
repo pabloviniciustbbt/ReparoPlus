d3efc68b954b940479c68ba71e8d52f2e275a39f
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2026-01-23 08:09:48 -0300

    feat: validar duplicidade de cpf e adicionar regras de ativação e inativação de atendente

--- src/main/java/com/pabloleal/ReparoPlus/services/AtendenteServices.java
+++ src/main/java/com/pabloleal/ReparoPlus/services/AtendenteServices.java
@@ -3,13 +3,16 @@ package com.pabloleal.ReparoPlus.services;
 import com.pabloleal.ReparoPlus.dto.PessoaCreateRequestDTO;
 import com.pabloleal.ReparoPlus.dto.PessoaResponseDTO;
 import com.pabloleal.ReparoPlus.dto.PessoaUpdateRequestDTO;
+import com.pabloleal.ReparoPlus.exceptions.EntidadeAtivaInativaException;
+import com.pabloleal.ReparoPlus.exceptions.EntidadeCadastradaException;
 import com.pabloleal.ReparoPlus.models.Atendente;
+import com.pabloleal.ReparoPlus.models.Pessoa;
 import com.pabloleal.ReparoPlus.repositories.AtendenteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import java.util.Optional;
 
 @Service
 public class AtendenteServices {
@@ -17,43 +20,74 @@ public class AtendenteServices {
     @Autowired
     private AtendenteRepository atendenteRepository;
 
-    public PessoaCreateRequestDTO cadastrarAtendente(PessoaCreateRequestDTO dados) {
+    public Atendente cadastrarAtendente(PessoaCreateRequestDTO dados) {
+
+        String cpfFormatado = Pessoa.formatarCpf(dados.cpf());
+
+        if (atendenteRepository.existsByCpf(cpfFormatado)){
+            throw new EntidadeCadastradaException("Atendente com CPF " + cpfFormatado + " já cadastrado");
+        }
+
         Atendente atendente = new Atendente(dados);
         atendenteRepository.save(atendente);
-        return dados;
+        return atendente;
     }
 
-    public PessoaUpdateRequestDTO atualizarAtendente(PessoaUpdateRequestDTO dados) {
-        Atendente atendente = atendenteRepository.getReferenceById(dados.id());
+    public Atendente atualizarAtendente(PessoaUpdateRequestDTO dados) {
+        Atendente atendente = atendenteRepository.findById(dados.id()).
+                orElseThrow(() -> new EntityNotFoundException("Atendente com ID " + dados.id() + " não encontrado"));
+
+        if (dados.cpf() != null){
+            String cpfFormatado = Pessoa.formatarCpf(dados.cpf());
+
+            if (atendenteRepository.existsByCpf(cpfFormatado)){
+                throw new EntidadeCadastradaException("Atendente com CPF " + cpfFormatado + " já cadastrado");
+            }
+        }
+
         atendente.atualizarPessoa(dados);
-        return new PessoaUpdateRequestDTO(atendente);
+        return atendente;
     }
 
     public void deletarAtendente(Long id) {
-        Atendente atendente = atendenteRepository.getReferenceById(id);
+        Atendente atendente = atendenteRepository.findById(id).
+                orElseThrow(() -> new EntityNotFoundException("Atendente com ID " + id + " não encontrado"));
+
+        if (!atendente.getAtivo()){
+            throw new EntidadeAtivaInativaException("Atendente com ID " + id + " já está inativo");
+        }
+
         atendente.deletarPessoa();
     }
 
-    public PessoaResponseDTO buscarAtendenteID(Long id) {
-        Optional<Atendente> atendente = atendenteRepository.findById(id);
+    public Atendente ativarAtendente(Long id){
+        Atendente atendente = atendenteRepository.findById(id).
+                orElseThrow(() -> new EntityNotFoundException("Atendente com ID " + id+ " não encontrado"));
 
-        if (atendente.isPresent()){
-            Atendente a = atendente.get();
-            return new PessoaResponseDTO(a.getId(), a.getCpf(), a.getNome(), a.getEmail(), a.getTelefone());
+        if (atendente.getAtivo()){
+            throw new EntidadeAtivaInativaException("Atendente com ID " + id + " já está ativo");
         }
 
-        return null;
+        atendente.ativarPessoa();
+        return atendente;
     }
 
-    public PessoaResponseDTO buscarAtendenteCPF(String cpf) {
-        Optional<Atendente> atendente = atendenteRepository.findByCpf(cpf);
+    public Atendente buscarAtendenteID(Long id) {
 
-        if (atendente.isPresent()){
-            Atendente a = atendente.get();
-            return new PessoaResponseDTO(a.getId(), a.getCpf(), a.getNome(), a.getEmail(), a.getTelefone());
-        }
+        Atendente atendente = atendenteRepository.findById(id).
+                orElseThrow(() -> new EntityNotFoundException("Atendente com ID " + id + " não encontrado"));
 
-        return null;
+        return atendente;
+    }
+
+    public Atendente buscarAtendenteCPF(String cpf) {
+
+        String cpfFormatado = Pessoa.formatarCpf(cpf);
+
+        Atendente atendente = atendenteRepository.findByCpf(cpfFormatado).
+                orElseThrow(() -> new EntityNotFoundException("Atendente com CPF " +  cpf + " não encontrado"));
+
+        return atendente;
     }
 
     public Page<PessoaResponseDTO> listarAtendentes(Pageable pageable) {

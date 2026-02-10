c498f830166965cbe60ebb60c9bcb0c87ef6f24f
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2026-02-10 08:21:45 -0300

    feat: adiciona service de produto
    - implementa cadastro com validacao de codigo ean duplicado
    - implementa atualizacao com verificacao de existencia
    - implementa soft delete de produto inativo
    - implementa reativacao de produto
    - adiciona tratamento de excecoes de entidade cadastrada e ativa/inativa

---
+++ src/main/java/com/pabloleal/ReparoPlus/services/ProdutoServices.java
@@ -0,0 +1,69 @@
+package com.pabloleal.ReparoPlus.services;
+
+import com.pabloleal.ReparoPlus.dto.produto.ProdutoCreateRequestDTO;
+import com.pabloleal.ReparoPlus.dto.produto.ProdutoUpdateRequestDTO;
+import com.pabloleal.ReparoPlus.exceptions.EntidadeAtivaInativaException;
+import com.pabloleal.ReparoPlus.exceptions.EntidadeCadastradaException;
+import com.pabloleal.ReparoPlus.models.Produto;
+import com.pabloleal.ReparoPlus.repositories.ProdutoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoServices {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto cadastrarProduto(ProdutoCreateRequestDTO produtoCreateRequestDTO) {
+
+        if (produtoRepository.existsByCodigoEan(produtoCreateRequestDTO.codigoEan())){
+            Produto produtoCadastrado = produtoRepository.findByCodigoEan(produtoCreateRequestDTO.codigoEan());
+            throw new EntidadeCadastradaException("Produto com Código EAN" + produtoCreateRequestDTO.codigoEan() +
+                    " já cadastrado com ID " + produtoCadastrado.getId());
+        }
+
+        Produto produto = new Produto(produtoCreateRequestDTO);
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    public Produto atualizarProduto(ProdutoUpdateRequestDTO produtoUpdateRequestDTO) {
+
+        Produto produto = produtoRepository.findById(produtoUpdateRequestDTO.id()).
+                orElseThrow(() -> new EntityNotFoundException("Produto com ID " + produtoUpdateRequestDTO.id() + " não encontrado"));
+
+        if (produtoRepository.existsByCodigoEan(produtoUpdateRequestDTO.codigoEan())){
+            Produto produtoCadastrado = produtoRepository.findByCodigoEan(produtoUpdateRequestDTO.codigoEan());
+            throw new EntidadeCadastradaException("Produto com EAN " + produtoUpdateRequestDTO.codigoEan() + " já cadastrado com ID " + produtoCadastrado.getId());
+        }
+
+        produto.atualizaProduto(produtoUpdateRequestDTO);
+
+        return produto;
+    }
+
+    public void deletarProduto(Long id) {
+        Produto produto = produtoRepository.findById(id).
+                orElseThrow(() -> new EntityNotFoundException("Produto com ID " + id + " não encontrado"));
+
+        if (!produto.isAtivo()){
+            throw new EntidadeAtivaInativaException("Produto com ID " + id + " já está inativo");
+        }
+
+        produto.deletarProduto();
+    }
+
+    public Produto ativarProduto(Long id) {
+        Produto produto = produtoRepository.findById(id).
+                orElseThrow(() -> new EntityNotFoundException("Produto com ID " + id + " não cadastrado"));
+
+        if (produto.isAtivo()){
+            throw new EntidadeAtivaInativaException("Produto com ID " + id + " já está ativo");
+        }
+
+        produto.ativarProduto();
+        return produto;
+    }
+}

06a56a9c6288885c98328e6694dcae064aa4112a
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2026-02-12 11:15:54 -0300

    feat: adiciona quantidadeEstoque ao ProdutoResponseDTO

--- src/main/java/com/pabloleal/ReparoPlus/dto/produto/ProdutoResponseDTO.java
+++ src/main/java/com/pabloleal/ReparoPlus/dto/produto/ProdutoResponseDTO.java
@@ -9,6 +9,7 @@ public record ProdutoResponseDTO(
         String descricao,
         String codigoEan,
         String fabricante,
+        Integer quantidadeEstoque,
         BigDecimal precoCusto,
         BigDecimal precoVenda
 ) {
@@ -19,6 +20,7 @@ public record ProdutoResponseDTO(
                 produto.getDescricao(),
                 produto.getCodigoEan(),
                 produto.getFabricante(),
+                produto.getQuantidadeEstoque(),
                 produto.getPrecoCusto(),
                 produto.getPrecoVenda()
         );

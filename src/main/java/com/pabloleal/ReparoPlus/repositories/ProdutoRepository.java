ff31623d35ecea8772d041bea56d3686969ef881
Pablo Vinicius <pabloviniciustbbt@gmail.com>
2026-02-13 10:48:12 -0300

    feat: adiciona consultas paginadas para produtos ativos e inativos no repositório

--- src/main/java/com/pabloleal/ReparoPlus/repositories/ProdutoRepository.java
+++ src/main/java/com/pabloleal/ReparoPlus/repositories/ProdutoRepository.java
@@ -1,10 +1,16 @@
 package com.pabloleal.ReparoPlus.repositories;
 
 import com.pabloleal.ReparoPlus.models.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     boolean existsByCodigoEan(String codigoEan);
 
     Produto findByCodigoEan(String codigoEan);
+
+    Page<Produto> findAllByAtivoTrue(Pageable pageable);
+
+    Page<Produto> findAllByAtivoFalse(Pageable pageable);
 }
